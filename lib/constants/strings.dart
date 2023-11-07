ebb0a2d5bda50ccd509f4afbc4e770cd837b2550
alonespace <jomcootle@gmail.com>
2023-11-08 00:01:32 +0700

    add: string in const

---
+++ lib/constants/strings.dart
@@ -0,0 +1,10 @@
+class MyString {
+  static const String mainTitle = "My Tasks";
+  static const String doneAllTask = "You have done all tasks";
+
+  static const String bottomAddTaskTitle = "Add Task";
+
+  static const String titleDeleteTaskDialog = "Delete Task";
+  static const String descDeleteTaskDialog =
+      "Are you sure you want to delete this task?";
+}
